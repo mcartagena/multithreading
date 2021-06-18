d269f7875c8570b42bda5d1316842cc869007024
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2021-06-18 13:13:07 -0400

    Adding Thread exception capture

--- thread/creation/example/Main.java
+++ thread/creation/example/Main.java
@@ -7,15 +7,27 @@ public class Main {
             // Code that will run in a new thread
             System.out.println("We are now in thread " + Thread.currentThread().getName());
             System.out.println("Current thread priority is: " + Thread.currentThread().getPriority());
+            throw new RuntimeException("Intentional Exception");
         });
 
         thread.setName("New worker thread");
 
         thread.setPriority(Thread.MAX_PRIORITY);
 
+        thread.setUncaughtExceptionHandler(
+                new Thread.UncaughtExceptionHandler() {
+                    @Override
+                    public void uncaughtException(Thread t, Throwable e){
+                        System.out.println("A critical error happened in Thread " + t.getName() + " the error is " + e.getMessage());
+                    }
+                }
+        );
+
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
         thread.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
+
+
 
         Thread.sleep(10000);
     }
